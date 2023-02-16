a7bb9c2a1430724e3765c056e5d602c40367d689
JNicolao <bororio96@gmail.com>
2023-02-16 05:41:55 +0000

    Corrections

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,10 +1,8 @@
--- List all shows and genres linked to show from 'hbtn_0d_tvshows'
--- If show doesn't have a genre, display NULL in genre column
--- Each record should display tv_shows.title, tv_genres.name
--- Results must be sorted in ascending order by show title
--- You can only use one SELECT statement
-SELECT s.title, g.name
-FROM tv_shows s
-LEFT JOIN tv_show_genres m ON s.id = m.show_id
-LEFT JOIN tv_genres g ON m.genre_id = g.id
-ORDER BY s.title ASC;
+-- Script that lists all shows and all genres
+SELECT tv_shows.title, tv_genres.name -- Query to join tables
+FROM tv_shows
+     LEFT JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     LEFT JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
