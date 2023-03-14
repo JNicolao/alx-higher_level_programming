577c90f9007f4abe316162577fc64551009786e9
JNicolao <bororio96@gmail.com>
2023-03-14 15:36:39 +0000

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (!newDict[dict[key]]) {
     newDict[dict[key]] = [];
   }
   newDict[dict[key]].push(key);
