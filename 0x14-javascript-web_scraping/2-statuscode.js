1198c13df7a80ba7e95442e32fb95029e8721e4f
JNicolao <bororio96@gmail.com>
2023-04-26 19:42:31 +0000

    Web Scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-
 const request = require('request');
-const MovieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + MovieId;
-request.get(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(body).title);
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
   }
 });
